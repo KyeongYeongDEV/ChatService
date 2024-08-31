7f39d91c6549afcd3779117b3582e24c99121b87
KyeongYeongDEV <yeong7098@naver.com>
2024-08-31 17:37:31 +0900

    feat : create frontend

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,34 +1,36 @@
 import createApp from './app';
 import  http from 'http';
-import  { Server } from "socket.io";
+import {Server} from 'socket.io';
 import dotenv from "dotenv";
 
 dotenv.config();
-//const  server = http.createServer(app);
-//const io = new Server(server);
-
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-
-//     socket.on('chat message', (msg)=>{
-//         io.emit('chat message', msg);
-//     })
-
-//     socket.on('disconnect', () =>{
-//         console.log('user disconnected');
-//     });
-
-// });
-
 const PORT = process.env.PORT;
+
 (async function serverStart() {
     const app = await createApp();
-    http.createServer(app).listen(PORT, () => {
-        console.log(`Server started on ${PORT}`);
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    io.on('connetion', (socket) => {
+        console.log('A user connected : ', socket.id);
+
+        socket.on('send_mesage', (data: { room: string | string[]; }) => {
+            console.log('Message received : ', data);
+            io.to(data.room).emit('receive_message', data);
+        });
+
+        socket.on('join_room', (room: any) => {
+            socket.join(room);
+            console.log(`User with ID : ${socket.id} joined room : ${room}`);
+        });
+
+        socket.on('disconnet', () => {
+            console.log('User disconnected : ', socket.id);
+        });
+    });
+    
+
+    server.listen(PORT, () => {
+        console.log(`Server started on ${PORT}`); 
     });
 })();
--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Nevbar';
+
+import Home from './pages/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+import ChatMain from './components/chats/ChatMain';
+import ChatWindow from './components/chats/ChatWindow';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chat/:userId/main" element={<ChatMain />} />
+          <Route path="/chat/:userId/chatwindow/:chatName" element={<ChatWindow />} />
+        </Routes>
+      </div>
     </div>
   );
 }
